6501492ad4b6a3765bf65a929a5cd1568f4b13a6
VictorneoX <victorrodrigues753@gmail.com>
2020-10-25 20:53:55 -0300

    Date Format fixed

--- utils/masks.js
+++ utils/masks.js
@@ -2,5 +2,9 @@ $(document).ready(function(){
   $(".cpf").mask("999.999.999-99");
   $(".rg").mask("99.999.999-9");
   $(".cel").mask('99999-9999');
-  $(".birth").text(moment($(".birth").text()).format('D/MM/yyyy'));
+
+  $(".birth").each(function(index) {
+    $(this).text(moment($(this).text()).format('D/MM/yyyy'));
+  });
+  
 });
