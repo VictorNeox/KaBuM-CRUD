230183c0a264dfeb02583801a1d85d5f049b570d
Victor Neox <victorrodrigues753@gmail.com>
2020-10-29 09:43:42 -0300

    JWT Tests

---
+++ jwtvalidation.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$token = $_GET['token'];
+
+$part = explode(".",$token);
+$header = $part[0];
+$payload = $part[1];
+$signature = $part[2];
+
+
+$valid = hash_hmac('sha256',"$header.$payload",'KaBuMCRUD',true);
+$valid = base64_encode($valid);
+echo $signature . '</br>';
+echo $valid . '</br>';
+
+$payload = base64_decode($payload);
+$payload = json_decode($payload);
+
+print_r($payload->uid);
+print_r($payload->access);
